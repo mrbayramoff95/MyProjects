41addc9a506d083fee9eebe0ed5ea2510e0bb036
mrbayramoff <mergen.bayramow@bk.ru>
2023-02-22 19:01:11 +0300

    Add lesson2

---
+++ src/lessons2/input_output.java
@@ -0,0 +1,19 @@
+package lessons2;
+
+import java.util.Scanner;
+
+public class input_output {
+    public static void main(String[] args) {
+        int i;
+        System.out.println("Введите число");
+        Scanner scanner = new Scanner(System.in);
+        i = scanner.nextInt();
+
+        if(i % 2 == 0) {
+            System.out.println("Число " + i + " четное");
+        } else {
+            System.out.println("Число " + i + " нечетное");
+
+        }
+    }
+}

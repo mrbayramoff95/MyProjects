68eecb23559c479b98bfe6e792eed9e58d283a5d
mrbayramoff <mergen.bayramow@bk.ru>
2023-02-27 19:56:03 +0300

    Add lesson5

--- src/lesson5/hw2ChessBoard.java
+++ src/lesson5/hw2ChessBoard.java
@@ -2,6 +2,16 @@ package lesson5;
 
 public class hw2ChessBoard {
     public static void main(String[] args) {
-
+        String[][] board = new String[8][8];
+        for (int i = 0; i < board.length; i++) {
+            System.out.println("\n");
+            for (int j = 0; j < board.length - 1; j++) {
+                if ((i + j) % 2 == 0)
+                    board[i][j] = "W";
+                else
+                    board[i][j] = "B";
+                System.out.println(" " + board[i][j]);
+            }
+        }
     }
 }
